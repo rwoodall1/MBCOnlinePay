9f90b351acc816d5bc0f79333da1f8b316694b76
rwoodall1 <randy@305spin.com>
2016-09-09 10:44:57 -0500

    Commit

--- online-pay/WebApp/app/main/admin/login.js
+++ online-pay/WebApp/app/main/admin/login.js
@@ -10,35 +10,53 @@ angular.module('app')
         function initialize() {
          
             $scope.passWord = '';
-            $scope.schoolCode=''
+            $scope.schoolCode = '';
             $scope.loginFailed = false;
             $scope.login = login;
             $scope.loggingIn = false;
         }
+        function setFormsToSubmitted() {
+            $scope.form.$setSubmitted();
+        }
 
+        function setAllFormInputsToDirty() {
+            for (var property in $scope.form) {
+                if ($scope.form.hasOwnProperty(property)) {
+                    if (property.indexOf("$") == -1) {
+                        $scope.form[property].$setDirty();
+                    }
+                }
+            }
+        }
         function login() {
-            $scope.paycodeNotFound = false;
-            $scope.loggingIn = true;
-         
-            InvoiceDataService.login($scope.schoolCode, $scope.passWord).then(function (response) {
-                if (!response.isSuccessful) {
-                    NotificationService.displayError('Login Failed');
-                    $scope.loginFailed = true;
-                    $scope.loggingIn = false;
-                    return;
-                }
-         
-                if (response.data) {
-                    $scope.loggingIn = false;
-                    $state.go('anon.administration', { invoicenumber: response.data.invno, schcode: response.data.schcode,schname:response.data.schname});
-                    return false;
-                } else {
-                    $scope.loggingIn = false;
-                    $scope.loginFailed = true;
-                    NotificationService.displayError('Login Failed');
-                }
 
-                $scope.loggingIn = false; });
+            setAllFormInputsToDirty()
+            setFormsToSubmitted()
+            if ($scope.form.$valid) {
+                $scope.paycodeNotFound = false;
+                $scope.loggingIn = true;
+
+                InvoiceDataService.login($scope.schoolCode, $scope.passWord).then(function (response) {
+                    if (!response.isSuccessful) {
+                        NotificationService.displayError('Login Failed');
+                        $scope.loginFailed = true;
+                        $scope.loggingIn = false;
+                        return;
+                    }
+
+                    if (response.data) {
+                        $scope.loggingIn = false;
+                        $state.go('anon.administration', { invoicenumber: response.data.invno, schcode: response.data.schcode, schname: response.data.schname });
+                        return false;
+                    } else {
+                        $scope.loggingIn = false;
+                        $scope.loginFailed = true;
+                        NotificationService.displayError('Login Failed');
+                    }
+
+                    $scope.loggingIn = false;
+                });
+            }
         }
     }
 ]);
